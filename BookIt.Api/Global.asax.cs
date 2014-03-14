5441e3449b690ceda5182dc388ebdb19a3ca05f9
myromanthen <roman.pavlushchenko@firstlinesoftware.com>
2014-03-14 16:44:18 +0400

    Initial upload of service api

---
+++ BookIt.Api/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace BookIt.Api
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+        }
+    }
+}

4fb40c377bca23fae777ab252370f0f47c0adb3d
Roman Pavlushchenko <myromanmusic@gmail.com>
2014-03-15 10:25:09 +0400

    Added MVC infrastructure

---
+++ BookIt.Api/App_Start/WebApiConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace BookIt.Api.App_Start
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional });
+        }
+    }
+}
--- BookIt.Api/Global.asax.cs
+++ BookIt.Api/Global.asax.cs
@@ -1,15 +1,15 @@
 ﻿using System.Web;
 using System.Web.Http;
 
+using BookIt.Api.App_Start;
+
 namespace BookIt.Api
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
 }

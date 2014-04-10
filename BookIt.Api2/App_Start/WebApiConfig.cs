e5232a9190dce4997d4d0899efb954972f5f257e
Roman Pavlushchenko <myromanmusic@gmail.com>
2014-04-10 08:34:38 +0400

    no comments

--- BookIt.Api2/App_Start/WebApiConfig.cs
+++ BookIt.Api2/App_Start/WebApiConfig.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
+﻿using System.Reflection;
 using System.Web.Http;
+
+using Autofac;
+using Autofac.Integration.WebApi;
+
+using BookIt.Business.RepositoriesImpl;
+
 using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
 
 namespace BookIt.Api2
 {
@@ -25,6 +27,23 @@ namespace BookIt.Api2
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+        }
+
+        public static void RegisterAutofacStuff(HttpConfiguration config)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<RepositoriesRegistrationModule>();
+            builder.RegisterModule<ApiRegistrationModule>();
+            builder.RegisterAssemblyTypes(
+                Assembly.GetExecutingAssembly())
+                .Where(t =>
+                       !t.IsAbstract && typeof(ApiController).IsAssignableFrom(t))
+                .InstancePerWebApiRequest();
+
+            var container = builder.Build();
+
+            var resolver = new AutofacWebApiDependencyResolver(container);
+            config.DependencyResolver = resolver;
         }
     }
 }

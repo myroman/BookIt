4b62ead3f67e7d79de750e201c09a86f6d6a4648
myromanthen <roman.pavlushchenko@firstlinesoftware.com>
2014-04-11 18:15:39 +0400

    #2: Expose resources+choose them in drop down

---
+++ BookIt.Api/Controllers/ResourcesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+
+using BookIt.Business.Abstract;
+using BookIt.Business.Models;
+
+namespace BookIt.Api.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/resources")]
+    public class ResourcesController : ApiController
+    {
+        private readonly IHubResourceRepository resourceRepository;
+
+        public ResourcesController(IHubResourceRepository resourceRepository)
+        {
+            this.resourceRepository = resourceRepository;
+        }
+
+        [Route("All")]
+        public IEnumerable<HubResource> GetAllResources()
+        {
+            return resourceRepository.GetList();
+        }
+    }
+}
